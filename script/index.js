6f2075aab76387f94fdcdb67ac8bbdb093fa7f45
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-02-07 20:01:21 +0600

    fetch data added in categories section

--- script/index.js
+++ script/index.js
@@ -3,8 +3,21 @@ function loadCategories() {
     .then((res) => res.json())
     .then((data )=> displayCategories(data.categories));
 }
+
 function displayCategories(categories) {
-  console.log(categories);
+  const categoriesButton=document.getElementById("categories-button");
+     for(let cot of categories)
+     {
+      console.log(cot)
+      const categoriesDiv=document.createElement('div');
+      categoriesDiv.innerHTML=`
+        <button class="btn btn-sm mr-5  hover:bg-red-800 hover:text-white">${cot.category}</button>
+      `
+      categoriesButton.appendChild(categoriesDiv);
+
+     }
 
 }
 loadCategories();
+
+// category_id: '1001', category: 'Music
