4a0267514f4a9cd79136e355353ad0475dba5983
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-02-08 14:44:18 +0600

    added fetch data and video card

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ function loadCategories() {
     .then((res) => res.json())
     .then((data )=> displayCategories(data.categories));
 }
+function loadVideoCard()
+{
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  .then(Response=>Response.json())
+  .then(data=>displayVideo(data.videos));
+}
 
 function displayCategories(categories) {
   const categoriesButton=document.getElementById("categories-button");
@@ -18,6 +24,37 @@ function displayCategories(categories) {
      }
 
 }
+function displayVideo(videos)
+{
+  const videoCard=document.getElementById("video-card");
+ // console.log(videos);
+ //{category_id: '1003', video_id: 'aaae', thumbnail: 'https://i.ibb.co/Yc4p5gD/inside-amy.jpg', title: 'Inside Amy Schumer', authors: Array(1), …}
+  for(let video of videos)
+  {
+    console.log(video);
+    const videoDiv=document.createElement("div");
+    
+    videoDiv.innerHTML=`
+       <div class="card bg-base-100 m-5 shadow-sm">
+        <figure>
+          <img
+            src="${video.thumbnail}"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">${video.title}</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    `
+    videoCard.appendChild(videoDiv);
+  }
+  
+}
 loadCategories();
+loadVideoCard();
 
 // category_id: '1001', category: 'Music
