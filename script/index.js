a0ee074c20e0a847ab117d4b754e411f0736c691
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-02-13 21:50:52 +0600

    added video details card

--- script/index.js
+++ script/index.js
@@ -1,71 +1,92 @@
 function loadCategories() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data )=> displayCategories(data.categories));
+    .then((data) => displayCategories(data.categories));
 }
-function loadVideoCard()
-{
+function loadVideoCard() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-  .then(Response=>Response.json())
-  .then(data=>
-    {
+    .then(Response => Response.json())
+    .then(data => {
+      removeActiveBtn();
       document.getElementById("all-btn").classList.add('active');
       displayVideo(data.videos)
-});
+    });
 }
-function removeActiveBtn()
-{
-  const activeBtn=document.getElementsByClassName('active');
+function removeActiveBtn() {
+  const activeBtn = document.getElementsByClassName('active');
   console.log(activeBtn);
-  for(let act of activeBtn )
-  {
+  for (let act of activeBtn) {
     act.classList.remove('active');
   }
 }
-const loadCategoriesVideo=(id)=>
-{
-   console.log(id);
-   const url =`https://openapi.programming-hero.com/api/phero-tube/category/${id}`
-   console.log(url)
-   fetch(url)
-   .then((res)=>res.json())
-   .then((data)=>
-    {
+const loadCategoriesVideo = (id) => {
+  console.log(id);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+  console.log(url)
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
       removeActiveBtn();
-      const clickedBtn=document.getElementById(`btn-${id}`)
+      const clickedBtn = document.getElementById(`btn-${id}`)
       console.log(clickedBtn);
       clickedBtn.classList.add("active");
       displayVideo(data.category);
 
     })
 }
-
-
-function displayCategories(categories) {
-  const categoriesButton=document.getElementById("categories-button");
-     for(let cot of categories)
-     {
-      console.log(cot)
-      const categoriesDiv=document.createElement('div');
-      categoriesDiv.innerHTML=`
-        <button id="btn-${cot.category_id}" onclick="loadCategoriesVideo(${cot.category_id})" class="btn btn-sm mr-5  hover:bg-red-800 hover:text-white">${cot.category}</button>
-      `
-      categoriesButton.appendChild(categoriesDiv);
-
-     }
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayVideoDetails(data.video))
+}
+function displayVideoDetails(vid) {
+  console.log(vid)
+  document.getElementById("video_details").showModal();
+  const modalView = document.getElementById("modal-view");
+  modalView.innerHTML =
+    `
+      <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img class="w-full "
+      src="${vid.thumbnail}"
+      alt="Show details" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${vid.title}</h2>
+    <p>${vid.description}</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+    
+  `
 
 }
-function displayVideo(videos)
-{
-  const videoCard=document.getElementById("video-card");
- // console.log(videos);
- //{category_id: '1003', video_id: 'aaae', thumbnail: 'https://i.ibb.co/Yc4p5gD/inside-amy.jpg', title: 'Inside Amy Schumer', authors: Array(1), …}
- //
- videoCard.innerHTML="";
-   if(videos.length==0)
-   {
+function displayCategories(categories) {
+  const categoriesButton = document.getElementById("categories-button");
+  for (let cot of categories) {
+    console.log(cot)
+    const categoriesDiv = document.createElement('div');
+    categoriesDiv.innerHTML = `
+        <button id="btn-${cot.category_id}" onclick="loadCategoriesVideo(${cot.category_id})" class="btn btn-sm mr-5  hover:bg-red-800 hover:text-white">${cot.category}</button>
+      `
+    categoriesButton.appendChild(categoriesDiv);
+
+  }
+
+}
+function displayVideo(videos) {
+  const videoCard = document.getElementById("video-card");
+  // console.log(videos);
+  //{category_id: '1003', video_id: 'aaae', thumbnail: 'https://i.ibb.co/Yc4p5gD/inside-amy.jpg', title: 'Inside Amy Schumer', authors: Array(1), …}
+  //
+  videoCard.innerHTML = "";
+  if (videos.length == 0) {
     console.log("innnnn");
-    videoCard.innerHTML=`
+    videoCard.innerHTML = `
       <div class="flex flex-col justify-center items-center p-30  col-span-full">
        <img class="w-[120px]" src="./Icon.png" alt="">
        <h1 class="text-2xl font-bold text-center">Oops!! Sorry, There is no content here</h1>
@@ -73,14 +94,13 @@ function displayVideo(videos)
 
     `
     return;
-      
-   }
-  for(let video of videos)
-  {
+
+  }
+  for (let video of videos) {
     console.log(video);
-    const videoDiv=document.createElement("div");
-    
-    videoDiv.innerHTML=`
+    const videoDiv = document.createElement("div");
+
+    videoDiv.innerHTML = `
     <div class="card bg-base-100 m-5">
         <figure class="relative">
           <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
@@ -116,11 +136,13 @@ function displayVideo(videos)
             <p class="text-sm text-gray-400">${video.others.views} view</p>
           </div>
         </div>
+        <button onclick="loadVideoDetails('${video.video_id}')"class="btn btn-block">Show details</button>
+        
       </div>
     `
     videoCard.appendChild(videoDiv);
   }
-  
+
 }
 loadCategories();
 
